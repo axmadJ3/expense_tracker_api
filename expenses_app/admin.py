5778c300d59dd07c8317a6e79cb860aeca7724a0
axmadJ3 <axmabdukarimov0304@outlook.com>
2024-09-24 21:53:54 +0500

    'admin'

--- expenses_app/admin.py
+++ expenses_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Expense
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('category', 'amount', 'date')
+    search_fields = ('category', 'description')
+    
